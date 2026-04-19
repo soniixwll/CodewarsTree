7f64dcf5a59d431927b8548d90b1c17a38bad84c
soniixwll <parobocha.pn@ucu.edu.ua>
2026-04-19 14:38:14 +0300

    Made function that showa all nodes by levels

--- Sort_Binary_Tree/solution.py
+++ Sort_Binary_Tree/solution.py
@@ -1,2 +1,28 @@
+from collections import deque
+
+class Node:
+    def __init__(self, L, R, n):
+        self.left = L
+        self.right = R
+        self.value = n
+
+# def has_children(node):
+
 def tree_by_levels(node):
-    return
+
+    if node is None:
+        return []
+
+    answear_list = []
+    queue = deque([node])
+    while queue:
+        part = queue.popleft()
+        answear_list.append(part.value)
+
+        if part.left:
+            queue.append(part.left)
+
+        if part.right:
+            queue.append(part.right)
+
+    return answear_list
