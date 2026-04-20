402a4fa9d80bb92086422b0cc132d67e143662ac
soniixwll <parobocha.pn@ucu.edu.ua>
2026-04-21 00:05:53 +0300

    Finished function

--- Delete_Node_in_BST/solution.py
+++ Delete_Node_in_BST/solution.py
@@ -7,5 +7,31 @@ class TreeNode:
         self.right = right
 
 class Solution:
+
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        ...
+        if root is None:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+            return root
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+            return root
+        else:
+
+            if not root.left:
+                return root.right
+            # elif not root.right:
+            elif not root.right:
+                return root.left
+            else:
+                head = root.right
+                while head.left:
+                    head = head.left
+
+                root.val = head.val
+
+                root.right = self.deleteNode(root.right , head.val)
+
+                return root
